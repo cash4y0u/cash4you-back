442508baceea3935b5eb94d1537aa3aa2c3cde63
cash4y0u <83921805+cash4y0u@users.noreply.github.com>
2025-05-18 22:09:41 -0300

    ajustes

--- database.py
+++ database.py
@@ -5,13 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_db_connection():
-    # ✅ Printar variáveis antes de conectar
-    print("🔍 HOST:", os.getenv("HOST"))
-    print("👤 USER:", os.getenv("USER"))
-    print("🔑 SECRET:", os.getenv("SECRET"))
-    print("📂 DATABASE:", os.getenv("DATABASE"))
-    print("📡 PORT:", os.getenv("PORT"))
-
     conn = pymysql.connect(
         host=os.getenv("HOST"),
         user=os.getenv("USER"),
